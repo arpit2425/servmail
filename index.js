3aa23d00ba66516b4a5c1dd9b74ea10f2989a59b
Arpit Trivedi <arpittrivedi2425@gmail.com>
2019-12-26 15:25:45 +0530

    added googleAuth

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express=require("express");
+const passport=require("passport");
+const key=require("./configs/keys");
+const googleStrategies=require("passport-google-oauth20");
 const app=express();
-app.get("/",(req,res)=>{
-    res.send({'bye':'buddy'});
+passport.use(new googleStrategies({
+    clientID:key.googleClientID,
+    clientSecret:key.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+},
+ (accessToken,refreshToken,profile,done)=>{
+     console.log('accessToken',accessToken);
 
-});
+    console.log('refreshToken',refreshToken);
+    console.log('Profile',profile);
+    }));
+// 
+// 
+
+app.get('/auth/google',passport.authenticate('google',{
+    scope:['profile','email']
+}))
+app.get('/auth/google/callback',passport.authenticate('google'));
 const port=process.env.PORT || 5000;
 
 app.listen(port);
